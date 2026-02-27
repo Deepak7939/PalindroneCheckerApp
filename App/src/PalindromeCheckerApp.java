888ee7a1c4eb7fd24fbb4b2976097ff513c14d06
Deepak7939 <sandudeepak7512@gmail.com>
2026-02-27 09:37:53 +0530

    Deepak Added UC2-HardCodePalindrome

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,26 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String text = "madam";
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(text + " is a palindrome.");
+        } else {
+            System.out.println(text + " is not a palindrome.");
+        }
+    }
+}
